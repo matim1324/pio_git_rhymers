6d647013a98803e3d37835502010f3c09ed85fc6
michalkedzia <222463@edu.p.lodz.pl>
2020-12-16 14:59:06 +0100

    Add constants to DefaultCountingOutRyhmer, Override to HanoiRhymer

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,29 +2,32 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-  private int[] numbers = new int[12];
+  public static final int SIZE = 12;
+  public static final int MAX = 11;
+  public static final int CURRENT= -1;
+  private final int[] numbers = new int[SIZE];
 
-  public int total = -1;
+  public int total = CURRENT;
 
   public void countIn(int in) {
     if (!isFull()) numbers[++total] = in;
   }
 
   public boolean callCheck() {
-    return total == -1;
+    return total == CURRENT;
   }
 
   public boolean isFull() {
-    return total == 11;
+    return total == MAX;
   }
 
   protected int peekaboo() {
-    if (callCheck()) return -1;
+    if (callCheck()) return CURRENT;
     return numbers[total];
   }
 
   public int countOut() {
-    if (callCheck()) return -1;
+    if (callCheck()) return CURRENT;
     return numbers[total--];
   }
 }
